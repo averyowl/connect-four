2e56b822f9fea92698a57d73fac4ee29bc54430b
Jacob Purvis <jacob@purvis.cc>
2025-08-14 16:47:49 -0700

    add mcts factory

--- mcts.py
+++ mcts.py
@@ -1,6 +1,7 @@
 import random
 from board import Board
 from game import Game
+from typing import Callable
 
 def ask(actions: list[int], state: Board, my_symbol: str, opponent_symbol: str) -> int:
     state.display_board()
@@ -8,31 +9,35 @@ def ask(actions: list[int], state: Board, my_symbol: str, opponent_symbol: str) 
     choice = input("choose a location: ")
     return int(choice) - 1
 
-def mcts(actions: list[int], state: Board, my_symbol: str, opponent_symbol: str) -> int:
-    best: dict[int, int] = {}
-    for i in actions:
-        best[i] = 0
+def mcts_factory(iterations: int) -> Callable[[list[int], Board, str, str], int]:
+    def inner(actions: list[int], state: Board, my_symbol: str, opponent_symbol: str) -> int:
+        best: dict[int, int] = {}
+        for i in actions:
+            best[i] = 0
 
-    for i in range(0, 500):
-        states: list[Board] = []
-        first_action = random.choice(actions)
-        new_state, won = state.take_action(my_symbol, first_action)
-        if(won == my_symbol):
-            best[first_action] += 1
-            continue
-        states.append(new_state)
-        my_turn = False
-        while(len(new_state.get_actions()) > 0):
-            new_state,won = new_state.take_action(my_symbol if my_turn else opponent_symbol, random.choice(new_state.get_actions()))
+        for i in range(0, iterations):
+            states: list[Board] = []
+            first_action = random.choice(actions)
+            new_state, won = state.take_action(my_symbol, first_action)
             if(won == my_symbol):
                 best[first_action] += 1
-                break
-            if(won == opponent_symbol):
-                best[first_action] -= 1
-                break
+                continue
             states.append(new_state)
-            my_turn = not my_turn
-    return int(max(best, key=best.get))
+            my_turn = False
+            while(len(new_state.get_actions()) > 0):
+                new_state,won = new_state.take_action(my_symbol if my_turn else opponent_symbol, random.choice(new_state.get_actions()))
+                if(won == my_symbol):
+                    best[first_action] += 1
+                    break
+                if(won == opponent_symbol):
+                    best[first_action] -= 1
+                    break
+                states.append(new_state)
+                my_turn = not my_turn
+        return int(max(best, key=best.get))
+    return inner
+
+mcts = mcts_factory(1000)
 
 def choose_random(actions: list[int], state: Board, my_symbol: str, opponent_symbol: str):
     return random.choice(actions)
